a054eaf0d85f500f650aed4b1b71ba7fd5714755
sujanchaudhary415 <108916550+sujanchaudhary415@users.noreply.github.com>
2026-04-08 10:06:49 +0545

    added ResponseEntity to delete feature

--- src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -2,10 +2,10 @@ package com.ecommerce.sb_ecom.controller;
 
 import com.ecommerce.sb_ecom.model.Category;
 import com.ecommerce.sb_ecom.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +34,21 @@ public class CategoryController {
     {
         categoryService.createCategory(category);
         return "Category added Successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId)
+    {
+        try
+        {
+          String status=categoryService.deleteCategory(categoryId);
+          return new ResponseEntity<>(status,HttpStatus.OK);
+
+        }
+        catch (ResponseStatusException e)
+        {
+            return new ResponseEntity<>(e.getReason(),e.getStatusCode());
+        }
     }
 
 }
--- src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
@@ -1,14 +1,16 @@
 package com.ecommerce.sb_ecom.service;
 
 import com.ecommerce.sb_ecom.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-
+    private Long nextId=1L;
 
     private List<Category> categories= new ArrayList<>();
 
@@ -19,6 +21,19 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
-       categories.add(category);
+        category.setCategoryId(nextId++);
+        categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+       Category category = categories.stream()
+                .filter(a-> a.getCategoryId().equals(categoryId))
+                .findFirst()
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Category Not Found"));
+
+
+       categories.remove(category);
+        return "CategoryId with "+categoryId+" deleted successfully";
     }
 }
